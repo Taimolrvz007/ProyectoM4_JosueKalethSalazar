e5313e37855a3a0a84f854d02e485d14483991da
Taimol_rvz <94725506+Taimolrvz007@users.noreply.github.com>
2026-05-05 12:47:52 -0500

    probando el inicio de google

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import Login from './pages/login';
 import Register from './pages/register';
 import Dashboard from './pages/dashboard';
 import './App.css';
+import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Ruta por defecto que redirige al login */}
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/login" />} /> 
         
         {/* Definición de tus rutas */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" 
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } 
+        />
         
         {/* Ruta para manejar errores 404 */}
         <Route path="*" element={<h2>404 - Not Found</h2>} />
